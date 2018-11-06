75ec18bbb8b018c3ef36b53c278ec11106c133c2
Ben Keith <bkeith@signalfx.com>
2018-11-06 17:48:13 -0500

    Fix style and try and enable dropwizard tests

--- dd-java-agent/instrumentation/servlet-3/src/main/java/datadog/trace/instrumentation/servlet3/Servlet3Advice.java
+++ dd-java-agent/instrumentation/servlet-3/src/main/java/datadog/trace/instrumentation/servlet3/Servlet3Advice.java
@@ -13,21 +13,19 @@ import io.opentracing.Tracer;
 import io.opentracing.propagation.Format;
 import io.opentracing.tag.Tags;
 import io.opentracing.util.GlobalTracer;
-
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import net.bytebuddy.asm.Advice;
 
 public class Servlet3Advice {
 
   @Advice.OnMethodEnter(suppress = Throwable.class)
   public static Scope startSpan(
-    @Advice.This final Object servlet, @Advice.Argument(0) final ServletRequest req) {
+      @Advice.This final Object servlet, @Advice.Argument(0) final ServletRequest req) {
     if (GlobalTracer.get().activeSpan() != null || !(req instanceof HttpServletRequest)) {
       // Tracing might already be applied by the FilterChain.  If so ignore this.
       return null;
@@ -35,23 +33,24 @@ public class Servlet3Advice {
 
     final HttpServletRequest httpServletRequest = (HttpServletRequest) req;
     final SpanContext extractedContext =
-      GlobalTracer.get()
-        .extract(
-          Format.Builtin.HTTP_HEADERS,
-          new HttpServletRequestExtractAdapter(httpServletRequest));
+        GlobalTracer.get()
+            .extract(
+                Format.Builtin.HTTP_HEADERS,
+                new HttpServletRequestExtractAdapter(httpServletRequest));
 
-    Tracer.SpanBuilder builder = GlobalTracer.get()
-      .buildSpan(
-        URLUtil.deriveOperationName(
-          httpServletRequest.getMethod(),
-          httpServletRequest.getRequestURL().toString(),
-          false))
-      .asChildOf(extractedContext)
-      .withTag(Tags.COMPONENT.getKey(), "java-web-servlet")
-      .withTag(Tags.HTTP_METHOD.getKey(), httpServletRequest.getMethod())
-      .withTag(Tags.HTTP_URL.getKey(), httpServletRequest.getRequestURL().toString())
-      .withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_SERVER)
-      .withTag("span.origin.type", servlet.getClass().getName());
+    Tracer.SpanBuilder builder =
+        GlobalTracer.get()
+            .buildSpan(
+                URLUtil.deriveOperationName(
+                    httpServletRequest.getMethod(),
+                    httpServletRequest.getRequestURL().toString(),
+                    false))
+            .asChildOf(extractedContext)
+            .withTag(Tags.COMPONENT.getKey(), "java-web-servlet")
+            .withTag(Tags.HTTP_METHOD.getKey(), httpServletRequest.getMethod())
+            .withTag(Tags.HTTP_URL.getKey(), httpServletRequest.getRequestURL().toString())
+            .withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_SERVER)
+            .withTag("span.origin.type", servlet.getClass().getName());
 
     String contextPath = httpServletRequest.getContextPath();
     if (!Strings.isNullOrEmpty(contextPath)) {
@@ -71,10 +70,10 @@ public class Servlet3Advice {
 
   @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
   public static void stopSpan(
-    @Advice.Argument(0) final ServletRequest request,
-    @Advice.Argument(1) final ServletResponse response,
-    @Advice.Enter final Scope scope,
-    @Advice.Thrown final Throwable throwable) {
+      @Advice.Argument(0) final ServletRequest request,
+      @Advice.Argument(1) final ServletResponse response,
+      @Advice.Enter final Scope scope,
+      @Advice.Thrown final Throwable throwable) {
 
     if (scope != null) {
       if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {

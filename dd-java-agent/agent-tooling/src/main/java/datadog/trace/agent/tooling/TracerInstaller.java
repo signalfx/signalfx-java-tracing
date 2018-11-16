03afba05d64a5ef69d8786b091522331b6b05131
Ben Keith <bkeith@signalfx.com>
2018-11-16 10:53:15 -0500

    Use ContextualScopeManager and send all traces by default

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/TracerInstaller.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/TracerInstaller.java
@@ -1,6 +1,8 @@
 /** Modified by SignalFx to use Jaeger Tracer instead of DDTracer */
 package datadog.trace.agent.tooling;
 
+import com.google.common.base.Strings;
+import datadog.opentracing.scopemanager.ContextualScopeManager;
 import io.jaegertracing.Configuration;
 import io.opentracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +12,18 @@ public class TracerInstaller {
   /** Register a global tracer if no global tracer is already registered. */
   public static synchronized void installGlobalTracer() {
     if (!io.opentracing.util.GlobalTracer.isRegistered()) {
-      final Tracer tracer = Configuration.fromEnv().getTracer();
+      Configuration conf = Configuration.fromEnv();
+
+      if (Strings.isNullOrEmpty(conf.getSampler().getType())) {
+        // Send all traces out by default since sampling happens downstream
+        conf =
+            conf.withSampler(
+                new Configuration.SamplerConfiguration().withType("const").withParam(1));
+      }
+
+      final Tracer tracer =
+          conf.getTracerBuilder().withScopeManager(new ContextualScopeManager()).build();
+
       try {
         io.opentracing.util.GlobalTracer.register(tracer);
       } catch (final RuntimeException re) {

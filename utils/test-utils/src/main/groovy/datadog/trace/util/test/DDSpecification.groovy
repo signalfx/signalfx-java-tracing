befc9c15f22a2473580f5de81b19f7180fd6d404
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2020-06-05 16:12:29 -0400

    Test flake fix from OTel f436499

--- utils/test-utils/src/main/groovy/datadog/trace/util/test/DDSpecification.groovy
+++ utils/test-utils/src/main/groovy/datadog/trace/util/test/DDSpecification.groovy
@@ -1,7 +1,10 @@
+// Modified by SignalFx
 package datadog.trace.util.test
 
 import net.bytebuddy.agent.ByteBuddyAgent
 import net.bytebuddy.agent.builder.AgentBuilder
+import net.bytebuddy.description.type.TypeDefinition
+import net.bytebuddy.description.type.TypeDescription
 import net.bytebuddy.dynamic.ClassFileLocator
 import net.bytebuddy.dynamic.Transformer
 import spock.lang.Specification
@@ -16,11 +19,33 @@ abstract class DDSpecification extends Specification {
   private static final String CONFIG = "datadog.trace.api.Config"
 
   static {
+    addByteBuddyRawSetting()
     makeConfigInstanceModifiable()
   }
 
   // Keep track of config instance already made modifiable
   private static isConfigInstanceModifiable = false
+
+  // Taken from https://github.com/open-telemetry/opentelemetry-java-instrumentation/commit/f436499
+  // this is a copy of same method from AgentInstaller
+  // it's needed here, because the test harness loads bytebuddy early
+  // and then it's too late to set the property in AgentInstaller
+  static void addByteBuddyRawSetting() {
+    final String savedPropertyValue = System.getProperty(TypeDefinition.RAW_TYPES_PROPERTY)
+    try {
+      System.setProperty(TypeDefinition.RAW_TYPES_PROPERTY, "true")
+      final boolean rawTypes = TypeDescription.AbstractBase.RAW_TYPES
+      if (!rawTypes) {
+        System.err.println("Too late to enable $TypeDefinition.RAW_TYPES_PROPERTY")
+      }
+    } finally {
+      if (savedPropertyValue == null) {
+        System.clearProperty(TypeDefinition.RAW_TYPES_PROPERTY)
+      } else {
+        System.setProperty(TypeDefinition.RAW_TYPES_PROPERTY, savedPropertyValue)
+      }
+    }
+  }
 
   static void makeConfigInstanceModifiable() {
     if (isConfigInstanceModifiable) {

1d279d262c2168b6592ad8c66a1ec151402a1c1a
Riley Martinez-Lynch <shunpiker@gmail.com>
2020-08-28 07:25:11 -0400

    Avoid doubly tracing MongoClientSettingsBuilder (#123)
    Avoid doubly tracing MongoClientSettingsBuilder
    
    Check for already injected TracingCommandListener before injecting.
    Patch derived from the fix for a similar issue in open-telemetry/opentelemetry-java-instrumentation.

--- dd-java-agent/instrumentation/mongo/driver-3.1/src/test/groovy/MongoClientTest.groovy
+++ dd-java-agent/instrumentation/mongo/driver-3.1/src/test/groovy/MongoClientTest.groovy
@@ -57,6 +57,30 @@ class MongoClientTest extends MongoBaseTest {
     renameService << [false, true]
   }
 
+  // Tests the fix for double-tracing, based on a similar fixed applied to opentelemetry-java-instrumentation
+  // TracingCommandListener might get added multiple times if ClientSettings are built using existing ClientSettings or when calling  a build method twice.
+  // This test asserts that duplicate traces are not created in those cases.
+  def "test create collection with already built ClientOptions"() {
+    setup:
+    def clientOptions = client.mongoClientOptions
+    def newClientOptions = MongoClientOptions.builder(clientOptions).build()
+    MongoDatabase db = new MongoClient(new ServerAddress("localhost", port), newClientOptions).getDatabase(dbName)
+
+    when:
+    db.createCollection(collectionName)
+
+    then:
+    assertTraces(1) {
+      trace(0, 1) {
+        mongoSpan(it, 0, "{\"create\":\"$collectionName\",\"capped\":\"?\"}")
+      }
+    }
+
+    where:
+    dbName = "test_db"
+    collectionName = "testCollection"
+  }
+
   def "test create collection no description"() {
     setup:
     MongoDatabase db = new MongoClient("localhost", port).getDatabase(dbName)

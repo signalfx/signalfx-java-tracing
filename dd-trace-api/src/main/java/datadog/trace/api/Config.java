40fc8d483c03914565c947c91dcf485eaf961227
John Bley <jbley@splunk.com>
2020-10-05 15:22:36 -0400

    Version 0.48.0-sfx2 (#128)
    - Camel instrumentation
    - Avoid doubly tracing MongoClientSettingsBuilder

--- dd-trace-api/src/main/java/datadog/trace/api/Config.java
+++ dd-trace-api/src/main/java/datadog/trace/api/Config.java
@@ -184,7 +184,7 @@ public class Config {
   public static final String TRACING_LIBRARY_KEY = "signalfx.tracing.library";
   public static final String TRACING_LIBRARY_VALUE = "java-tracing";
   public static final String TRACING_VERSION_KEY = "signalfx.tracing.version";
-  public static final String TRACING_VERSION_VALUE = "0.48.0-sfx1";
+  public static final String TRACING_VERSION_VALUE = "0.48.0-sfx2";
 
   private static final boolean DEFAULT_TRACE_ENABLED = true;
   public static final boolean DEFAULT_INTEGRATIONS_ENABLED = true;
